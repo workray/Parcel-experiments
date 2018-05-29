60f3c77bb4db9dc0a1119db3e95ad1ea3e634040
Raymond <workrayz@gmail.com>
2018-05-28 20:51:32 -0400

    TypographyJS and Building for Production

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
 import React from "react";
 import styles from "./styles.scss";
 
-export default () => <div className={styles.title}> Lalala </div>
+export default () => <div className={styles.title}><h1>Hello</h1><p>lorem ipsum</p></div>
